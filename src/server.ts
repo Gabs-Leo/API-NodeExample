35c9fff09d253e668275f6d33be0f3ad39533ced
gabriel-scaled <gabrieldemoraistaveira@gmail.com>
2024-02-22 14:34:48 -0300

    added base port

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ import Express from 'express';
 const app = Express();
 
 app.get("/", (request, response) => response.json({result: "hello world"}))
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 4000);
